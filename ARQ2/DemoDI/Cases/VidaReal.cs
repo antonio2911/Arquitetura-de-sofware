cfdf1159b34e5f53be31528254b4bb1fa11cd421
Antonio Junio Pardim <antonio.pardim@totvs.com.br>
2023-03-19 18:30:17 -0300

    alterações git

--- ARQ2/DemoDI/Cases/VidaReal.cs
+++ ARQ2/DemoDI/Cases/VidaReal.cs
@@ -20,10 +20,11 @@ namespace DemoDI.Cases
     {
         public void AdicionarCliente(Cliente cliente)
         {
-            // Faz algo
+           // Faz algo
+          Console.WriteLine("Ola mundo");
         }
     }
-
+  // tanto cliente service como cliente repositorio implementa o mesmo metodo
     public interface IClienteServices
     {
         void AdicionarCliente(Cliente cliente);
@@ -31,13 +32,16 @@ namespace DemoDI.Cases
 
     public class ClienteServices : IClienteServices
     {
+      //Adiciona outra interface
         private readonly IClienteRepository _clienteRepository;
 
+        // no construtor e passado um contrato de um repositorio no lugar do contrato de serviço
         public ClienteServices(IClienteRepository clienteRepository)
         {
             _clienteRepository = clienteRepository;
         }
 
+        //faz referencia ao proprio contrato
         public void AdicionarCliente(Cliente cliente)
         {
             _clienteRepository.AdicionarCliente(cliente);
